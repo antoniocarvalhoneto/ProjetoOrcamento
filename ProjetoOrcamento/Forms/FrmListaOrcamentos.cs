67f83bd28230385103354b7162a99047ad655d6f
Antonio <neto.antonio1301@gmail.com>
2026-06-08 22:51:10 -0300

    adicionando servico e preparando sistema para novas feats

--- ProjetoOrcamento/Forms/FrmListaOrcamentos.cs
+++ ProjetoOrcamento/Forms/FrmListaOrcamentos.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ProjetoOrcamento
+namespace ProjetoOrcamento.Forms
 {
-    internal class FrmOrcamentos
+    internal class FrmListaOrcamentos
     {
     }
 }
